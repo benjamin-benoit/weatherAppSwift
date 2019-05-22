731c8a1a43a1e34fac5845351c51c53137a1d771
Benjamin Benoit <benjaminbenoitfr@gmail.com>
2019-05-22 09:33:48 +0200

    mapViewController and weatherViewController

--- weatherAppSwift/MapView.swift
+++ weatherAppSwift/MapView.swift
@@ -9,13 +9,37 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var selectedCoordinate: CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for cities in CitiesData.list {
+            let pin = MKPointAnnotation()
+            pin.coordinate = cities.coordinates
+            mapView.addAnnotation(pin)
+        }
     }
-
-
+    
+    //    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView){
+    //        performSegue(withIdentifier: "annotation_ID", sender: nil)
+    //    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        self.selectedCoordinate = view.annotation as? CLLocationCoordinate2D
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //        let filteredLocations = CitiesData.filter { (location) -> Bool in
+        //            return (location.latitude == self.selectedAnnotation?.coordinate.latitude && location.longitude == self.selectedAnnotation?.coordinate.longitude)
+        //        }
+        //        let selectedLocation = filteredLocations.first
+        if segue.identifier == "annotation_ID" {
+            let weatherViewController = segue.destination as! WeatherViewController
+            weatherViewController.location = selectedCoordinate ?? CLLocationCoordinate2D()
+        }
+    }
+    
 }
-
