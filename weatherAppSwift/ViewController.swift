6deedab4ce43e837abbed9943f488b85936371c9
Benjamin Benoit <benjaminbenoitfr@gmail.com>
2019-05-22 08:19:21 +0200

    Initial Commit

---
+++ weatherAppSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherAppSwift
+//
+//  Created by Benjamin BENOIT on 22/05/2019.
+//  Copyright © 2019 Benjamin Ibrahima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
