e91fc7277282d8dc24eac6f89b9fbe2537b118c5
ibrahima <ibrahima@dansoko>
2019-05-24 09:32:12 +0200

    heheh

--- weatherAppSwift/controllers/WeatherViewController.swift
+++ weatherAppSwift/controllers/WeatherViewController.swift
@@ -24,13 +24,15 @@ class WeatherViewController: UIViewController, UITableViewDataSource, UITableVie
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.backgroundColor = UIColor.clear
         requestWeather()
         self.title = cities
     }
     override func viewWillAppear(_ animated: Bool) {
         let backgroundImage = UIImage(named: "rain.png")
         tableView.backgroundView = UIImageView(image: backgroundImage)
-        tableView.backgroundColor = UIColor.clear
+        tableView.contentMode = .scaleAspectFit
+      
         
     }
     func requestWeather() {
@@ -75,41 +77,49 @@ class WeatherViewController: UIViewController, UITableViewDataSource, UITableVie
         switch (indexPath.section) {
         case 0:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell", for: indexPath) as? HeaderCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(withWeather: weather?.currently.icon ?? "", temperature: weather?.currently.temperature ?? 0, summary: weather?.currently.summary ?? "")
                 return cell
             }
         case 1:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "titleHourlyCell", for: indexPath) as? TitleCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(withWeather: weather?.hourly.summary ?? "error")
                 return cell
             }
         case 2:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "hourlyCell", for: indexPath) as? HourlyForecastCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(withWeather: weather?.hourly.data[indexPath.row].icon ?? "", temperature: weather?.hourly.data[indexPath.row].temperature ?? 0, time: weather?.hourly.data[indexPath.row].time ?? 0, humidity: weather?.hourly.data[indexPath.row].humidity ?? 0)
                 return cell
             }
         case 3:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "titleDailyCell", for: indexPath) as? TitleCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(withWeather: weather?.daily.summary ?? "error")
                 return cell
             }
         case 4:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "dailyCell", for: indexPath) as? DailyForecastCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(withWeather: weather?.daily.data[indexPath.row].icon ?? "null", minTemperature: weather?.daily.data[indexPath.row].temperatureMin ?? 0.0, time: weather?.daily.data[indexPath.row].time ?? 00, maxTemperature: weather?.daily.data[indexPath.row].temperatureMax ?? 0.0)
                 return cell
             }
         case 5:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "titleExtraCell", for: indexPath) as? TitleCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(withWeather: "Extra Informations")
                 return cell
             }
         case 6:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "firstExtrasCell", for: indexPath) as? ExtraInfosCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(leftTitle: "Humidity", leftValue: "\(Int((weather?.currently.humidity ?? 0)*100)) %", rightTitle: "WindSpeed", rightValue: "\(Int(weather?.currently.windSpeed ?? 0)) km/h")
                 return cell
             }
         case 7:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "secondExtrasCell", for: indexPath) as? ExtraInfosCell {
+                cell.backgroundColor = UIColor.clear
                 cell.configure(leftTitle: "Pressure", leftValue: "\(Int(weather?.currently.pressure ?? 0)) hPa", rightTitle: "UV Index", rightValue: "\(weather?.currently.uvIndex ?? 0)")
                 return cell
             }
